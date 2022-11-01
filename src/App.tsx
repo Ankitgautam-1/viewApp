f65b701c6457dd76e70971fd400e30833be467a1
ankit gautam <ankitgautam8898@gmail.com>
2022-11-01 14:17:41 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <iframe
+      style={{
+        height: "100vh",
+        width: "100vw",
+        border: "none",
+      }}
+      src="http://localhost:3000/?user_name=ankit&user_email=ankit@freshservice.com&user_id=-0001"
+    ></iframe>
+  );
+}
+
+export default App;
